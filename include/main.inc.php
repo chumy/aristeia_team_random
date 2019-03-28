758f7ded0bf07f0b9f07fb73a7484f8d05531fdd
chumy <chumy@chumy.net>
2019-03-28 11:26:30 +0100

    fixes

--- include/main.inc.php
+++ include/main.inc.php
@@ -1,18 +1,18 @@
 <?php
 
 
-define( 'DB_HOST', 'localhost' ); // set database host
-define( 'DB_USER', 'root' ); // set database user
-define( 'DB_PASS', 'realdb' ); // set database password
-define( 'DB_NAME', 'aristeia_panel' ); // set database name
+define( 'DB_HOST', 'XXXXX' ); // set database host
+define( 'DB_USER', 'XXXX' ); // set database user
+define( 'DB_PASS', 'XXXX' ); // set database password
+define( 'DB_NAME', 'XXXXX' ); // set database name
 define( 'SEND_ERRORS_TO', 'XXXXX@XXXX' ); //set email notification email address
 define( 'DISPLAY_DEBUG', true ); //display db errors?
 
 require_once('PDO.inc.php');
 
 // RUTAS ABSOLUTAS
-define("local","/media/Datos/export/chumy/Proyectos/Aristeia/escaleta/aristeia_team_random");
-define('ROOT',$_SERVER['DOCUMENT_ROOT']);
+define("local","/home/chumynet/public_html/aristeia");
+define('ROOT',$_SERVER['DOCUMENT_ROOT'].'/aristeia');
 define('INCLUDES', ROOT.'/include');
 define('JS', ROOT.'/js');
 define('STYLES', ROOT.'/styles');
@@ -25,7 +25,7 @@ define("UPLOAD_DIR",local.'/uploads/');
 define('ADM_MODULES', ROOT.'/admin/modules');
 
 //RUTAS RELATIVAS
-define('WROOT','/');
+define('WROOT','/aristeia/');
 define('WINCLUDES', '/include');
 define('WJS', '/js');
 define('WSTYLES', '/styles');
